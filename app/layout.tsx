ef0188353857c85842600b68ee13bc6b2f141623
Benjamin31600 <ruggieribenjamin34@icloud.com>
2026-09-08 23:04:51 -0400

    feat: strengthen MyCoco technical SEO metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,25 +6,36 @@ import { site } from "@/lib/site";
 export const metadata: Metadata = {
   metadataBase: new URL(site.url),
   title: {
-    default: "MyCoco | Garderies, CPE et solutions de garde au Québec",
+    default: "MyCoco | Trouver la bonne solution de garde au Québec",
     template: "%s | MyCoco",
   },
   description: site.description,
   applicationName: "MyCoco",
   category: "childcare",
+  keywords: [
+    "garderie Québec",
+    "CPE Québec",
+    "garderie Mirabel",
+    "garderie Blainville",
+    "garderie Boisbriand",
+    "garderie Saint-Eustache",
+    "garderie Sainte-Thérèse",
+    "solution de garde",
+    "garde d'enfants",
+  ],
   robots: { index: true, follow: true },
   openGraph: {
     type: "website",
     locale: "fr_CA",
     siteName: "MyCoco",
-    title: "MyCoco | Garderies, CPE et solutions de garde au Québec",
+    title: "MyCoco | Trouver la bonne solution de garde au Québec",
     description: site.description,
     url: site.url,
     countryName: "Canada",
   },
   twitter: {
     card: "summary_large_image",
-    title: "MyCoco | Garderies, CPE et solutions de garde au Québec",
+    title: "MyCoco | Trouver la bonne solution de garde au Québec",
     description: site.description,
   },
 };
@@ -45,15 +56,17 @@ export default function RootLayout({
       name: "Quebec",
       containedInPlace: { "@type": "Country", name: "Canada" },
     },
+    potentialAction: {
+      "@type": "SearchAction",
+      target: `${site.url}/fr/garderies?ville={search_term_string}`,
+      "query-input": "required name=search_term_string",
+    },
   };
 
   return (
     <html lang="fr-CA">
       <body>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-        />
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
         {children}
       </body>
     </html>
